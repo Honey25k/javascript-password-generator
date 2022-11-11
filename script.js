14a50c6dd5922c38229d1209ed2fb68b9f5d3e92
honeytech <honey93k@gmail.com>
2022-11-10 18:11:58 -0800

    update: created prompts for password critera

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 // Assignment Code
+
+
+var lowerCase = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]; 
+
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8","9",];
+
+var specialChar = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
+
+var userChoiceLength;
+
+var userChoice;
+
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+userChoiceLength = window.prompt("Choose a password thats between 8 - 128 character long");
+
+if(userChoiceLength < 8 || userChoiceLength > 128) {
+    window.alert("Password criteria not met, try again.");
+} else {
+
+var userOptionLowerCase = window.confirm("Do you want to use lower case letters?");
+var userOptionUpperCase = window.confirm("Do you want to use upper case letters?");
+var userOptionNumber = window.confirm("Do you want to use numbers?");
+var userOptionSpecialChar = window.confirm("Do you want to use any special characters?");
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
